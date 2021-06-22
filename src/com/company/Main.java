aebe3994748e1cc743c15584aeeead245282e6c6
rensiqi1 <409480920@qq.com>
2021-06-22 22:03:25 +0800

    hello,world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello,World");
         System.out.println("Hello,world,Hello,World");
+        System.out.println("jjsf interface");
     }
 }
 
